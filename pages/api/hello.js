d904f18295cee2588a32b014acd572f158620bf9
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-02 12:44:03 -0600

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+	if (req.method !== "GET") {
+		res.status(405).send({
+			message: "Only GET requests allowed"
+		});
+		return
+	}
+	res.status(200).send({
+		test: req
+	});
 }

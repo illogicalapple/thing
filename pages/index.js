b8c9432ac8f622956a02b4a1c74ff6d75d18d9b5
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-02 12:36:44 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.status(405).send({ message: 'Only POST requests allowed' })
-    return
-  }
-
-  const body = JSON.parse(req.body)
-
-  // the rest of your code
+export default function Home() {
+	return (<h1>hello</h1>)
 }
